73bb6c567375f66b612563e66699232c81c0d279
alisonpeard <alison.peard@gmail.com>
2026-01-01 16:14:12 +0000

    (wip) school figs

--- analysis/scripts/school_disruption.py
+++ analysis/scripts/school_disruption.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 relevant_hazards = ["pluvial", "fluvial", "coastal", "landslide"]#, "cyclone"]
 crit_path = "~/Desktop/tza_school_roads_edge_criticality.csv"
 hazdir_base = "/Users/alison/Local/github/oia-tanzania-2025/results/risk/tza_roads_edges"
+
 def extract_hazard_info(hazcol:str) -> tuple[str, str, str, int]:
     """Extract hazard, epoch, scenario, and return period from hazard column name."""
     prefix, parts = hazcol.split("-")
@@ -37,52 +38,50 @@ if __name__ == "__main__":
         for file in hazfiles:
             print(file)
             df = gpd.read_parquet(file)
-            df["subregion"] = os.path.basename(os.path.dirname(file))
-            haz_dfs.append(df)
+            subregion = os.path.basename(os.path.dirname(file))
+            df["subregion"] = subregion
+            damage_cols = [col for col in df.columns if col.startswith("damage-")]
 
-        haz_df = pd.concat(haz_dfs).sort_index()
-        damage_cols = [col for col in haz_df.columns if col.startswith("damage-")]
+            hazards = []
+            epochs = []
+            scenarios = []
+            rps = []
+            stats = []
+            total_isolated = np.zeros(len(damage_cols), dtype=float)
+            total_rerouted = np.zeros(len(damage_cols), dtype=float)
+            total_weighted_detour = np.zeros(len(damage_cols), dtype=float)
 
-        hazards = []
-        epochs = []
-        scenarios = []
-        rps = []
-        stats = []
-        total_isolated = np.zeros(len(damage_cols), dtype=float)
-        total_rerouted = np.zeros(len(damage_cols), dtype=float)
-        total_weighted_detour = np.zeros(len(damage_cols), dtype=float)
+            for i, col in enumerate(damage_cols):
+                print(f"Processing columnn {i} / {len(damage_cols)} for {subregion}: {col}")
+                damaged = df[df[col] > 0].copy()
+                disrupted = damaged[[col]].join(
+                    criticality[["base_flux", "detoured_flux", "isolated_flux", "weighted_detour"]],
+                    how="left"
+                )
+                prefix, hazard, epoch, scenario, rp, stat = extract_hazard_info(col)
+                hazards.append(hazard)
+                epochs.append(epoch)
+                scenarios.append(scenario)
+                rps.append(rp)
+                stats.append(stat)
 
-        for i, col in enumerate(damage_cols):
-            print(f"Processing columnn {i} / {len(damage_cols)}: {col}")
-            damaged = haz_df[haz_df[col] > 0].copy()
-            disrupted = damaged[[col]].join(
-                criticality[["base_flux", "detoured_flux", "isolated_flux", "weighted_detour"]],
-                how="left"
-            )
+                total_isolated[i] = disrupted["isolated_flux"].sum()
+                total_rerouted[i] = disrupted["detoured_flux"].sum()
+                total_weighted_detour[i] = disrupted["weighted_detour"].sum()
 
-            prefix, hazard, epoch, scenario, rp, stat = extract_hazard_info(col)
-            hazards.append(hazard)
-            epochs.append(epoch)
-            scenarios.append(scenario)
-            rps.append(rp)
-            stats.append(stat)
-
-            total_isolated[i] = disrupted["isolated_flux"].sum()
-            total_rerouted[i] = disrupted["detoured_flux"].sum()
-            total_weighted_detour[i] = disrupted["weighted_detour"].sum()
-
-        summary = pd.DataFrame({
-            "hazard": hazards,
-            "epoch": epochs,
-            "scenario": scenarios,
-            "rp": rps,
-            "stat": stats,
-            "total_isolated": total_isolated,
-            "total_rerouted": total_rerouted,
-            "total_weighted_detour": total_weighted_detour
-        })
-        summary = summary.sort_values(by=["hazard", "epoch", "scenario", "rp", "stat"])
-        summary_dfs.append(summary)
+            summary = pd.DataFrame({
+                "subregion": subregion,
+                "hazard": hazards,
+                "epoch": epochs,
+                "scenario": scenarios,
+                "rp": rps,
+                "stat": stats,
+                "total_isolated": total_isolated,
+                "total_rerouted": total_rerouted,
+                "total_weighted_detour": total_weighted_detour
+            })
+            summary = summary.sort_values(by=["subregion", "hazard", "epoch", "scenario", "rp", "stat"])
+            summary_dfs.append(summary)
     
     summary_final = pd.concat(summary_dfs)
     summary_final.to_csv("~/Desktop/tza_school_roads_hazard_disruption_summary.csv", index=False)
